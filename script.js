8e68f898bd981a8f91e8dde0056f1e0a87d8c028
Haiylu <Haiylu.Kibret@gmail.com>
2020-10-09 15:33:34 -0400

    Removed dots within index.html

--- script.js
+++ script.js
@@ -23,14 +23,4 @@ function setTheme(mode){
 	if(mode == 'blue'){
 		document.getElementById('theme-style').href = 'blue.css'
 	}
-
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
-
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
-
-	localStorage.setItem('theme', mode)
 }
